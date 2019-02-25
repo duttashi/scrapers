0766c251255930137fe433c7a5d93249d0b152d5
Ashish Dutt <ashish.dutt8@gmail.com>
2019-02-25 09:48:26 +0800

    modified script: Basic EDA
    - split ItemDateTime variable to ItemDate and ItemTime
    - replace value "today" with current date in iDate
    - remove the time in iDate

--- src/R/scrape_mudah_EDA.R
+++ src/R/scrape_mudah_EDA.R
@@ -9,3 +9,20 @@
 rm(list = ls())
 # Load the data
 mudah.data<- read.csv("data/mudah_data.csv", header = TRUE, sep = ",")
+
+# Load the required libraries
+library(tidyverse)
+
+# Basic EDA
+
+# split ItemDateTime variable to ItemDate and ItemTime
+df <- separate(mudah.data, col = itemDateTime, into = c("iDate", "iTime"),sep = ",")
+# replace value "today" with current date in iDate
+df<- df %>%
+  mutate(iDate=replace(iDate, iDate=="today",date()))
+# remove the time in iDate
+df<- df %>%
+  mutate(iDate=replace(iDate, iDate=="Mon Feb 25 09:13:34 2019","Mon Feb 25 2019"))
+
+# Extract the day, month, year, time from iDate
+str(df$iDate)
