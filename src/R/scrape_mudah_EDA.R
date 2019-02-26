b025bef55b693ff3bbe80ff6bd34d75c3e18cf61
Ashish Dutt <ashish.dutt8@gmail.com>
2019-02-26 15:46:54 +0800

    modified script (added new code)
    - collapse levels in `itemCat` variable.
    - collapse levels in `itemCondt` variable.
    - imputed missing data with predictive mean modeling method.
    - cross tabulate the complete data `df.cmplt` for documentation purpose.
    - write the clean data frame, `df.cmplt` to disk.

--- src/R/scrape_mudah_EDA.R
+++ src/R/scrape_mudah_EDA.R
@@ -1,8 +1,8 @@
-# objective: Preliminary data analysis of the scraped data from mudah.my
-# required data frame: `df.final`
+# objective: Exploratory data analysis of the scraped data from mudah.my
 # required data file: `data/mudah_data.csv`
 # script name: scrape_mudah_EDA.R
 # script create date: 24/2/2019
+# script last modified date: 26/2/2019
 # script author: ashish dutt
 
 # clean the workspace
@@ -17,12 +17,106 @@ library(tidyverse)
 
 # split ItemDateTime variable to ItemDate and ItemTime
 df <- separate(mudah.data, col = itemDateTime, into = c("iDate", "iTime"),sep = ",")
+View(df)
 # replace value "today" with current date in iDate
 df<- df %>%
   mutate(iDate=replace(iDate, iDate=="today",date()))
 # remove the time in iDate
 df<- df %>%
-  mutate(iDate=replace(iDate, iDate=="Mon Feb 25 09:13:34 2019","Mon Feb 25 2019"))
+  mutate(iDate=replace(iDate, iDate=="Tue Feb 26 13:52:11 2019","Tue Feb 26 2019"))
 
-# Extract the day, month, year, time from iDate
-str(df$iDate)
+# Separate the day, month, year, time from iDate
+df<-separate(df, col = iDate, into = c("iDay","iMonth","iDate","iYear"),
+                      sep = " ")
+table(df$iYear) # constant value drop it
+table(df$iDate)# constant value drop it
+table(df$iDay) # variance, dont drop
+table(df$iMonth) # variance, dont drop
+table(df$iTime) # variance, dont drop
+
+# dropping cols iYear, iDay, iDate
+colnames(df)
+# reference: https://stackoverflow.com/questions/35839408/r-dplyr-drop-multiple-columns
+df.new<- df %>%
+  select(-c(iYear,iDate))
+
+# Look & clean the other variables like itemArea, itemCat, itemCondt
+table(df.new$itemCat) # categories like Full-time,Johor,Kuala Lumpur,Kelantan,Melaka,Pahang,Part-time,Penang, Perak,Putrajaya, Sabah, Selangor don't make sense.
+                      # Aggregrating these categories into miscellaneous
+# collapse levels in variable itemCat
+levels(df.new$itemCat)<- list(
+  misc = c("Full-time","Johor","Kelantan","Kuala Lumpur","Melaka","Negeri Sembilan","Pahang",
+           "Part-time","Penang","Perak","Putrajaya","Sabah","Selangor",
+           "Freelance","Internship","Business for Sale","Jobs",
+           "Contract","Food","Others"),
+  
+  Accessories = c("Accessories for Phones & Gadgets","Car Accessories & Parts",
+                  "Computers & Accessories","Motorcycle Accessories & Parts",
+                  "Other Accessories & Parts","Watches & Fashion Accessories"
+                  ),
+  Gadgets = c("Cameras & Photography","Mobile Phones & Gadgets",
+              "TV/Audio/Video","Games & Consoles"),
+  HomeEquipmt = c("Bed & Bath","Furniture & Decoration","Home Appliances & Kitchen",
+                  "Garden Items"),
+  GenEquipmt = c("Air Conditioning","Hobby & Collectibles",
+                 "Music Instruments","Professional/Business Equipment"),
+  Vehicles = c("Commercial Vehicle & Boats","Motorcycles"),
+  Clothing = c("Clothes","Shoes"),
+  GenServc = c("Cleaning","Events","Tutoring","Pest Control","Moms & Kids","Pets","Plumbing",
+                  "Printing","Renovation","Sports & Outdoors",
+                  "Tours and Holidays","Transport","Wedding"),
+  healthServc = c("Health & Beauty")
+  )
+levels(df.new$itemCat)
+table(df.new$itemCat)
+
+# clean the itemCondt column
+# replace 'Second-Hand','Pre-owned' with 'Used'
+levels(df.new$itemCondt)
+table(df.new$itemCondt)
+levels(df.new$itemCondt)<- list(
+  New = c("New"),
+  Used = c("Pre-owned (Used)","Second-hand (Used)","Used")
+)
+
+# Look at the data structure
+str(df.new)
+
+# Visualize missing data 
+library(VIM)
+aggr_plot <- aggr(df.new, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, 
+                  labels=names(df.new), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
+
+# Impute missing values with Predictive Mean Matching
+library(mice)
+tempData <- mice(df.new,m=5,maxit=10,method='pmm',seed=500)
+summary(tempData)
+# complete data
+df.cmplt<- complete(tempData,2)
+colSums(is.na(df.cmplt))
+
+dim(df.cmplt) # 20,500 adverts in 8 columns
+colnames(df.cmplt)
+table(df.cmplt$iMonth) # 20,000 observations in Feb and 500 in July
+
+# Cross Tab
+library(Hmisc)
+summary(~., data = df.cmplt, fun = table)
+# max ads are from KL (23% n=4613) then Kedah (22% n=4612), Selangor(12%, n=2555), Penang(10% 2052)
+# max ads are in Car Accessories & Parts category(31% n=6354), Clothes(13%, n=2607), Home Appliances & Kitchen(10%, n=2076), Jobs(8%, n=1564), Sports & Outdoors(8%, n=1561) 
+# max ads are for New items(68% n=13850) followed by Used items(32% n=6650)
+# There are 1080 ads priced over a million, possible outliers. check further
+boxplot(df.cmplt$itemPrice)
+str(df.cmplt)
+
+
+table(df.cmplt$itemPrice)
+table(df.cmplt$iMonth)
+table(df.cmplt$itemArea) 
+table(df.cmplt$iTime) # 20,000 ads posted between 9:57 am to 9:59 am on 26th Feb 2019
+table(df.cmplt$itemArea, df.cmplt$iTime) # Kedah posted highest number of ads between 9:57-958 am, followed by KL
+table(df.cmplt$itemArea, df.cmplt$itemCat) # KL posted max adverts in "Car accessories & parts" followed by Kedah
+
+
+# write the clean file to disk
+write.csv(df.cmplt,"data/mudha_data_clean.csv")
